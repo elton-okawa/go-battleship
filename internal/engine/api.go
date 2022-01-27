3bbf258ca0dfd8d10aef063ffc50a78dde10435e
Elton Okawa <elton.okawa@gmail.com>
2022-01-27 20:33:38 -0300

    feat: add shoot function

--- internal/engine/api.go
+++ internal/engine/api.go
@@ -4,9 +4,16 @@ import (
 	"fmt"
 )
 
+// TODO persist it somewhere
+var board Board
+
 func StartGame() {
 	fmt.Println("Start game")
 
-	board := Init()
+	board = Init()
 	fmt.Println(board)
+}
+
+func Shoot(row, col int) (bool, int) {
+	return board.Shoot(row, col)
 }
--- internal/engine/board.go
+++ internal/engine/board.go
@@ -6,36 +6,65 @@ import (
 )
 
 type Board struct {
-	board [][]rune
-	size  int
+	placement [][]rune
+	state     [][]rune
+	size      int
+	shipCount int
 }
 
 var EMPTY = '-'
+var SINGLE_SQUARE_SHIP = 'S'
+var HIT = 'X'
+var MISS = '0'
 
 func Init() Board {
 	size := 8
-	b := make([][]rune, size)
-
-	for i := 0; i < size; i++ {
-		b[i] = make([]rune, size)
-	}
+	shipCount := 3
+	placement := emptyMap(size)
+	state := emptyMap(size)
 
 	board := Board{
-		board: b,
-		size:  size,
+		placement: placement,
+		state:     state,
+		size:      size,
+		shipCount: shipCount,
 	}
 
-	for i := 0; i < board.size; i++ {
-		for j := 0; j < board.size; j++ {
-			board.board[i][j] = EMPTY
-		}
-	}
-
-	for i := 0; i < 3; i++ {
-		placeShip(&board, 'S', 1)
+	for i := 0; i < board.shipCount; i++ {
+		placeShip(&board, SINGLE_SQUARE_SHIP, 1)
 	}
 
 	return board
+}
+
+func emptyMap(size int) [][]rune {
+	m := make([][]rune, size)
+
+	for i := 0; i < size; i++ {
+		m[i] = make([]rune, size)
+	}
+
+	for i := 0; i < size; i++ {
+		for j := 0; j < size; j++ {
+			m[i][j] = EMPTY
+		}
+	}
+
+	return m
+}
+
+func (board *Board) Shoot(row, col int) (bool, int) {
+	hit := board.placement[row][col] == SINGLE_SQUARE_SHIP
+	if hit {
+		board.state[row][col] = HIT
+
+		// TODO bigger ships are not sinked right away
+		board.shipCount -= 1
+	} else {
+		board.state[row][col] = MISS
+	}
+
+	return hit, board.shipCount
 }
 
 func placeShip(board *Board, char rune, size int) {
@@ -47,8 +76,8 @@ func placeShip(board *Board, char rune, size int) {
 		row := rand.Intn(board.size)
 		col := rand.Intn(board.size)
 
-		if board.board[row][col] == EMPTY {
-			board.board[row][col] = char
+		if board.placement[row][col] == EMPTY {
+			board.placement[row][col] = char
 			positioned = true
 		}
 	}
@@ -58,7 +87,7 @@ func (b Board) String() string {
 	var sb strings.Builder
 	for i := 0; i < b.size; i++ {
 		for j := 0; j < b.size; j++ {
-			sb.WriteRune(b.board[i][j])
+			sb.WriteRune(b.placement[i][j])
 			sb.WriteRune(' ')
 		}
 		sb.WriteRune('\n')
