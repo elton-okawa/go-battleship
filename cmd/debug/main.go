4ffbf18cc3e3e559f0fbc7e281cfb008bb825812
Elton Okawa <elton.okawa@gmail.com>
2022-03-06 18:02:48 -0300

    feat: add debug command to test json persistence

--- cmd/debug/main.go
+++ cmd/debug/main.go
@@ -5,12 +5,18 @@ import (
 	"fmt"
 	"os"
 
+	"elton-okawa/battleship/internal/database"
 	"elton-okawa/battleship/internal/use_case"
 )
 
 func main() {
 
 	debugCommand := flag.NewFlagSet("debug", flag.ExitOnError)
+	databaseCommand := flag.NewFlagSet("db", flag.ExitOnError)
+
+	operationPtr := databaseCommand.String("operation", "", "Operation to perform in DB (Required)")
+	idPtr := databaseCommand.String("id", "", "File id")
+	fieldPtr := databaseCommand.String("field", "", "Simple field")
 
 	if len(os.Args) <= 1 {
 		fmt.Println("Please, provide a subcommand")
@@ -22,6 +28,8 @@ func main() {
 	switch command {
 	case "debug":
 		debugCommand.Parse(os.Args[2:])
+	case "db":
+		databaseCommand.Parse(os.Args[2:])
 	default:
 		fmt.Println("Subcommand not found")
 		flag.PrintDefaults()
@@ -30,5 +38,28 @@ func main() {
 
 	if debugCommand.Parsed() {
 		use_case.StartGame()
+	} else if databaseCommand.Parsed() {
+		handleDbCommand(*operationPtr, *idPtr, *fieldPtr)
+	}
+}
+
+type DebugModel struct {
+	id    string
+	field string
+}
+
+func handleDbCommand(operation string, id string, field string) {
+	fmt.Printf("Db command (operation: %s, id: %s, field: %s)\n", operation, id, field)
+	db := database.JsonDatabase{Filepath: "./db/index.json"}
+
+	if operation == "" {
+		fmt.Println("Operation must be passed")
+		os.Exit(1)
+	}
+
+	switch operation {
+	case "save":
+		dm := DebugModel{id: id, field: field}
+		db.Save(dm)
 	}
 }
