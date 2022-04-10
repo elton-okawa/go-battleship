81f8a30675be4710531c97d61fae78d3f65cb65b
Elton Okawa <elton.okawa@gmail.com>
2022-04-10 11:25:43 -0300

    feat: add accounts controller

---
+++ internal/interface_adapter/controller/account.go
@@ -0,0 +1,17 @@
+package controller
+
+import "elton-okawa/battleship/internal/use_case/account"
+
+type AccountController struct {
+	useCase account.AccountUseCase
+}
+
+func NewAccountController(a account.AccountUseCase) AccountController {
+	return AccountController{
+		useCase: a,
+	}
+}
+
+func (c AccountController) CreateAccount(res account.AccountOutput, login, password string) {
+	c.useCase.CreateAccount(res, login, password)
+}
