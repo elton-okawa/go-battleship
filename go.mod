72f224a60c199365a9920fd3bb8552fc7370fe4b
Elton Okawa <elton.okawa@gmail.com>
2022-04-10 18:32:20 -0300

    feat: add openapi code gen

--- go.mod
+++ go.mod
@@ -1,8 +1,21 @@
 module elton-okawa/battleship
 
-go 1.13
+go 1.18
 
 require (
 	github.com/google/uuid v1.3.0
 	golang.org/x/crypto v0.0.0-20220408190544-5352b0902921
+)
+
+require github.com/labstack/echo/v4 v4.7.2
+
+require (
+	github.com/labstack/gommon v0.3.1 // indirect
+	github.com/mattn/go-colorable v0.1.11 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
+	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
+	golang.org/x/sys v0.0.0-20211103235746-7861aae1554b // indirect
+	golang.org/x/text v0.3.7 // indirect
 )
--- internal/api/main.go
+++ internal/api/main.go
@@ -1,57 +1,20 @@
-// Routing based on Axel Wagner's ShiftPath approach:
-// https://blog.merovius.de/2017/06/18/how-not-to-use-an-http-router.html
-
-package api
+package Api
 
 import (
-	"elton-okawa/battleship/internal/api/accounts"
-	"elton-okawa/battleship/internal/api/games"
-	"elton-okawa/battleship/internal/api/router"
-	"elton-okawa/battleship/internal/database"
-	"elton-okawa/battleship/internal/interface_adapter/controller"
-	"elton-okawa/battleship/internal/interface_adapter/presenter/rest"
-	"elton-okawa/battleship/internal/use_case"
-	"elton-okawa/battleship/internal/use_case/account"
-	"net/http"
+	"github.com/labstack/echo/v4"
 )
 
-type App struct {
-	routers map[string]router.Router
+type BattleshipImpl struct{}
+
+func (*BattleshipImpl) CreateAccount(ctx echo.Context) error {
+
+	return nil
 }
 
-func Init() *App {
-	gameHandler := use_case.NewGame(
-		&database.DefaultDatabase,
-	)
+func SetupHandler() *echo.Echo {
+	var app BattleshipImpl
+	e := echo.New()
+	RegisterHandlers(e, &app)
 
-	gamesController := controller.NewGamesController(
-		gameHandler,
-	)
-
-	accountDao := database.NewAccountDao("./db/accounts.json")
-	accountsHandler := account.NewAccountUseCase(
-		accountDao,
-	)
-	accountsController := controller.NewAccountController(
-		accountsHandler,
-	)
-
-	return &App{
-		routers: map[string]router.Router{
-			"games":    games.NewGamesRouter(gamesController),
-			"accounts": accounts.NewAccountsRouter(accountsController),
-		},
-	}
-}
-
-func (app *App) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
-	var resource string
-	resource, r.URL.Path = router.ShiftPath(r.URL.Path)
-
-	presenter := rest.NewRestApiPresenter(rw)
-	if router, exist := app.routers[resource]; exist {
-		router.Route(presenter, r)
-	} else {
-		presenter.Error("Not Implemented", http.StatusNotImplemented)
-	}
+	return e
 }
